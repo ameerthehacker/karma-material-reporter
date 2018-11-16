ba4a3f48d452e8a61a2d7c3ff8cf82435da7c13d
ameerthehacker <ameerjhanprof@gmail.com>
2018-11-16 16:04:35 +0530

    feat (UI): Design basic home page

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
   private initSubscription: Subscription;
