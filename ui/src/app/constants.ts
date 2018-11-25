50b4cab7d34ad483fa517a96167c5f65015de246
ameerthehacker <ameerjhanprof@gmail.com>
2018-11-25 13:44:56 +0530

    feat: add logo for all supported browsers

--- ui/src/app/constants.ts
+++ ui/src/app/constants.ts
@@ -1,9 +1,17 @@
 const baseImagePath = '/src/assets/images';
 const chromeLogo = `${baseImagePath}/chrome-logo.png`;
 const fireFoxLogo = `${baseImagePath}/firefox-logo.png`;
+const safariLogo = `${baseImagePath}/safari-logo.png`;
+const phantomJSLogo = `${baseImagePath}/phantomjs-logo.png`;
+const operaLogo = `${baseImagePath}/opera-logo.png`;
+const ieLogo = `${baseImagePath}/ie-logo.png`;
 
 export const browserImages = [
   { name: 'HeadlessChrome', image: chromeLogo },
   { name: 'Chrome', image: chromeLogo },
-  { name: 'Firefox', image: fireFoxLogo }
+  { name: 'Firefox', image: fireFoxLogo },
+  { name: 'Safari', image: safariLogo },
+  { name: 'PhantomJS', image: phantomJSLogo },
+  { name: 'Opera', image: operaLogo },
+  { name: 'IE', image: ieLogo }
 ];
